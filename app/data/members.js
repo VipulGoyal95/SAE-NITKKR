071454e38968de0cef3095cd09d35553736a2035
DivyanshGarg96783 <bharatgarg685@gmail.com>
2026-04-22 21:01:22 +0530

    changed a photo of a member

--- app/data/members.js
+++ app/data/members.js
@@ -544,7 +544,7 @@ export const Details3yr = [
   },
   {
     id: 49,
-    img: '/assets/images/Teammembers/KSHITIZ GUPTA.webp',
+    img: '/assets/images/Teammembers/Kshitz.jpg',
     name: 'Kshitiz Gupta',
     Team: 'Nitrox',
     Department: 'Electrical',
