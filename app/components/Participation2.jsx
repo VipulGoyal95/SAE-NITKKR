47be6f3d2250adaf083f7903a24a40e3c7b5b1c1
Vipul Goyal <134921215+VipulGoyal95@users.noreply.github.com>
2026-04-27 21:55:09 +0530

    changed content of participations

--- app/components/Participation2.jsx
+++ app/components/Participation2.jsx
@@ -16,10 +16,12 @@ const Participation2 = () => {
   }
 
   const nitroxcontent={
-    top:"eBAJA",
-    top_content: "eBAJA is a premier electric vehicle design competition organized by the Society of Automotive Engineers India (SAE India). In this dynamic competition, student teams from universities across the country design, build, and race electric-powered off-road vehicles, emphasizing sustainability, innovation, and performance.",
-    bottom: "ATVC",
-    bottom_content: "ATVC (All-Terrain Vehicle Challenge) is a prestigious competition in India where student teams from universities across the nation design, build, and test all-terrain vehicles. The competition provides a platform for students to apply their engineering knowledge, enhance their practical skills, and push the limits of off-road vehicle technology."
+      top:"eBAJA",
+      top_content: "eBAJA is the flagship electric ATV competition organized by the SAE India, where student teams from universities across the country conceptualize, design, manufacture, and test their electric ATVs. The competition challenges participants to balance sustainability, engineering innovation, cost effectiveness, and on-track performance while working through real-world automotive design constraints.",
+      top_subcontent: "For the 2025–26 season, our team participated in eBAJA 2026 held at NATRAX, Pithampur, and achieved: AIR 46 Overall, AIR 35 in Statics with the highlights being AIR 13 in Manufacturing Excellence, AIR 20 in Sustainability.",
+      bottom: "ATVC",
+      bottom_content: "ATVC (All-Terrain Vehicle Championship) is a premier student engineering competition in India organized by INFI LEAGUE MOTORSPORTS, where teams from universities across the country design, manufacture, and validate their own all-terrain vehicles.",
+      bottom_subcontent: "In the 2025–26 season, we marked a significant milestone by returning to the electric category for the first time since 2020, delivering an exceptional performance with podium finishes across multiple categories: AIR 9 Overall. AIR 3 in Statics, AIR 7 in Endurance, AIR 3 in Design, AIR 4 in Business Plan and AIR 6 in Cost",
   }
 
   const imagesAcclerons = {
@@ -34,7 +36,7 @@ const Participation2 = () => {
   const team = isNitroxPage ? nitroxcontent : acceleronsContent;
   const images = isNitroxPage ? imagesNitrox : imagesAcclerons;
   return (
-    <div className="relative px-8 w-full max-w-6xl h-[2000px] mx-auto bg-black max-[1220px]:w-[90%] max-[530px]:w-[95%] max-[530px]:h-[930px] max-[460px]:h-[1500px]">
+    <div className={`relative px-8 w-full max-w-6xl h-[2000px] mx-auto bg-black max-[1220px]:w-[90%] max-[530px]:w-[95%] ${isNitroxPage ? 'max-[530px]:h-[1710px] max-[420px]:h-[1750px] max-[400px]:h-[1790px] max-[376px]:h-[1850px]' : 'max-[530px]:h-[1500px] max-[460px]:h-[1600px]'}`}>
       {/* Heading */}
       <motion.h2 
         className="absolute w-full text-center top-5 left-1/2 transform -translate-x-1/2 text-4xl font-bold text-white mb-4 max-[482px]:text-3xl"
@@ -47,7 +49,7 @@ const Participation2 = () => {
       </motion.h2>
 
       {/* Top‑Left Photo */}
-      <div className="absolute top-24 left-0 w-full h-[22%] overflow-hidden">
+      <div className={`absolute top-24 left-0 w-full ${isNitroxPage?"h-[22.5%]":"h-[22%]"} overflow-hidden`}>
         <Image
           src={images.top}
           alt="Drift race"
@@ -57,7 +59,7 @@ const Participation2 = () => {
       </div>
 
       {/* Top‑Right "SUPRA" Card */}
-      <div className="absolute top-[24.5%] left-0 w-full h-[26%]">
+      <div className={`absolute  left-0 w-full ${isNitroxPage ? 'h-[26.5%] top-[24%]' : 'h-[26%] top-[24.5%]'}`}>
         {/* Mint border PNG */}
         <Image
           src="/mobile upborder.webp"
@@ -67,14 +69,14 @@ const Participation2 = () => {
         />
         {/* Content panel inside the border */}
         <motion.div 
-          className={`absolute inset-6 rounded-lg p-4 flex flex-col justify-center max-[1038px]:p-3 max-[757px]:p-1 max-[657px]:p-0 max-[460px]:inset-4`}
+          className={`absolute inset-6 rounded-lg p-4 flex flex-col justify-center max-[1038px]:p-3 max-[757px]:p-1 max-[657px]:p-0 ${isNitroxPage ? 'max-[600px]:p-0 max-[460px]:p-0 max-[460px]:inset-4' : 'max-[460px]:inset-4'}`}
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true, margin: "-100px" }}
           transition={{ duration: 0.5, delay: 0.2 }}
         >
           <motion.h3 
-            className="text-[50px] text-center font-semibold text-white max-[1038px]:text-[40px] max-[896px]:text-[38px] max-[858px]:text-[35px] max-[802px]:text-[32px] max-[713px]:text-[29px] max-[602px]:text-[27px]"
+            className={`text-[50px] text-center font-semibold text-white max-[1038px]:text-[40px] max-[896px]:text-[38px] max-[858px]:text-[35px] max-[802px]:text-[32px] max-[713px]:text-[29px] max-[602px]:text-[27px] ${isNitroxPage ? 'max-[460px]:text-[25px]' : ''}`}
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true, margin: "-100px" }}
@@ -83,7 +85,7 @@ const Participation2 = () => {
             {team.top}
           </motion.h3>
           <motion.p 
-            className="mt-4 text-[18px] text-white leading-relaxed text-justify max-[896px]:mt-2 max-[858px]:text-[17px] max-[714px]:text-[16px] max-[657px]:text-[14px]"
+            className={`mt-4 text-[18px] text-white leading-relaxed text-justify max-[896px]:mt-2 max-[858px]:text-[17px] max-[714px]:text-[16px] ${isNitroxPage ? 'max-[657px]:text-[13px] max-[460px]:mt-0' : 'max-[657px]:text-[14px]'}`}
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true, margin: "-100px" }}
@@ -91,20 +93,23 @@ const Participation2 = () => {
           >
             {team.top_content}
           </motion.p>
-          {/* <motion.p 
-            className="mt-2 text-[16px] border-x-[5px] px-2 border-[red] text-white leading-relaxed text-justify"
-            initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true, margin: "-100px" }}
-            transition={{ duration: 0.5, delay: 0.4 }}
-          >
-            {team.top_subcontent}
-          </motion.p> */}
+          {
+            isNitroxPage &&
+            <motion.p 
+              className="mt-2 text-[16px] text-white leading-relaxed text-justify max-[460px]:text-[13px]"
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true, margin: "-100px" }}
+              transition={{ duration: 0.5, delay: 0.4 }}
+            >
+              {team.top_subcontent}
+            </motion.p>
+          }
         </motion.div>
       </div>
 
       {/* Bottom‑Left "Formula Bharat" Card */}
-      <div className="absolute top-[46.5%] left-0 w-full h-[26%] max-[530px]:bottom-15">
+      <div className={`absolute top-[46.5%] left-0 w-full ${isNitroxPage ? "h-[29%]": "h-[26%]"} max-[530px]:bottom-15`}>
         <Image
           src="/mobile lowborder.webp"
           alt="Formula Bharat border"
@@ -128,7 +133,7 @@ const Participation2 = () => {
             {team.bottom}
           </motion.h3>
           <motion.p 
-            className="mt-4 text-[18px] text-white leading-relaxed text-justify max-[896px]:mt-2 max-[858px]:text-[17px] max-[714px]:text-[16px] max-[657px]:text-[14px]"
+            className={`mt-4 text-[18px] text-white leading-relaxed text-justify max-[896px]:mt-2 max-[858px]:text-[17px] max-[714px]:text-[16px] max-[657px]:text-[14px] ${isNitroxPage? "max-[460px]:text-[13px]":""}`}
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true, margin: "-100px" }}
@@ -136,20 +141,23 @@ const Participation2 = () => {
           >
             {team.bottom_content}
           </motion.p>
-          {/* <motion.p 
-            className="mt-2 w-[70%] text-[16px] border-x-[5px] px-2 border-[red] text-white leading-relaxed text-justify"
-            initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true, margin: "-100px" }}
-            transition={{ duration: 0.5, delay: 0.4 }}
-          >
-            {team.bottom_subcontent}
-          </motion.p> */}
+          {
+            isNitroxPage && 
+            <motion.p 
+              className={`mt-2 text-[16px] text-white leading-relaxed text-justify max-[460px]:text-[13px] ${isNitroxPage? "max-[460px]:text-[13px]": ""}`}
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true, margin: "-100px" }}
+              transition={{ duration: 0.5, delay: 0.4 }}
+            >
+              {team.bottom_subcontent}
+            </motion.p>
+          }
         </motion.div>
       </div>
 
       {/* Bottom‑Right Photo */}
-      <div className={`absolute bottom-0 right-0 w-full rounded-xl overflow-hidden ${isNitroxPage? "h-[26%] max-[530px]:bottom-20": "h-[28%] max-[530px]:bottom-13"}`}>
+      <div className={`absolute bottom-15 right-0 w-full rounded-xl overflow-hidden ${isNitroxPage? "h-[25%] max-[530px]:bottom-11": "h-[28%] max-[530px]:bottom-13"}`}>
         <Image
           src={images.bottom}
           alt="Formula car"
